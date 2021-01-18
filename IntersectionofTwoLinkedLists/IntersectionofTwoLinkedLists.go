52c966b1a9380a5733bf93208921280b6934e245
Joseph <362489140@qq.com>
2021-01-18 09:19:36 +0800

    commit

---
+++ IntersectionofTwoLinkedLists/IntersectionofTwoLinkedLists.go
@@ -0,0 +1,27 @@
+package IntersectionofTwoLinkedLists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func GetIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pA := headA
+	pB := headB
+	for pA != pB {
+		if pA == nil {
+			pA = headB
+		} else {
+			pA = pA.Next
+		}
+		if pB == nil {
+			pB = headA
+		} else {
+			pB = pB.Next
+		}
+	}
+	return pA
+}
